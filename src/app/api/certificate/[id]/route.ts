6dab0e489fa88351d5a80144f9825e8810205e46
Agus Rakhmat Haryanto <xtkr2.03.agusrakhmatharyanto@gmail.com>
2025-05-22 14:05:44 +0700

    update

--- src/app/api/certificate/[id]/route.ts
+++ src/app/api/certificate/[id]/route.ts
@@ -1,13 +1,18 @@
 import { db } from "@/lib/firebaseConfig";
 import { NextResponse } from "next/server";
 import { doc, updateDoc, deleteDoc } from "firebase/firestore";
-import { RouteContext } from "@/types/cv";
 
-export async function PUT(req: Request, context: RouteContext) {
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
   try {
-    const { id } = context.params;
+    const { id } = params;
     if (!id) {
-      return NextResponse.json({ message: "Missing certificate id" }, { status: 400 });
+      return NextResponse.json(
+        { message: "Missing certificate id" },
+        { status: 400 }
+      );
     }
 
     const body = await req.json();
@@ -17,15 +22,24 @@ export async function PUT(req: Request, context: RouteContext) {
     return NextResponse.json({ message: "certificate updated", id });
   } catch (err) {
     console.error("Error updating certificate:", err);
-    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
 
-export async function DELETE(_: Request, context: RouteContext) {
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
   try {
-    const { id } = context.params;
+    const { id } = params;
     if (!id) {
-      return NextResponse.json({ message: "Missing certificate id" }, { status: 400 });
+      return NextResponse.json(
+        { message: "Missing certificate id" },
+        { status: 400 }
+      );
     }
 
     const docRef = doc(db, "certificate", id);
@@ -34,6 +48,9 @@ export async function DELETE(_: Request, context: RouteContext) {
     return NextResponse.json({ message: "certificate deleted", id });
   } catch (err) {
     console.error("Error deleting certificate:", err);
-    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
   }
 }
